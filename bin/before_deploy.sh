0d20a049d4e73786e2ba09c93a15ec87a0ba74b3
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-05-07 02:05:04 +0900

    Retry fixing deploy script

--- bin/before_deploy.sh
+++ bin/before_deploy.sh
@@ -4,5 +4,5 @@ set -ev
 
 # Deploy built files to gh-pages
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-git fetch --unshallow
+git fetch origin
 git checkout gh-pages .gitignore
