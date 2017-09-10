39b4f0686fe5b54e9502c9f1a908c6f350a361b5
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-09-10 09:18:54 +0900

    Fix ai_vs_ai

--- api/ai_vs_ai.js
+++ api/ai_vs_ai.js
@@ -4,15 +4,22 @@ const ai2 = require('./ai');
 
 const board = new Board();
 
+board.on('win', (winner) => {
+	console.log(`AI${winner + 1} won`)
+})
+
 for (;;) {
 	const point = board.getCurrentPoint();
 	if (!point || point.movableDirections.size === 0) {
 		break;
 	}
 	const ai = [ai1, ai2][board.activePlayer];
-	const hist = board.trace.reduce((prev, curr) => prev.concat(curr), []);
-	console.log(hist);
-	const aiDir = ai(hist);
+	let hist = board.trace.reduce((prev, curr) => prev.concat(curr), []);
+	console.log(JSON.stringify(hist));
+	if (board.activePlayer === 1) {
+		hist = hist.map(([x, y]) => [8 - x, 8 - y]);
+	}
+	const aiDir = (ai(hist) + board.activePlayer * 4) % 8;
 	console.log(`AI${board.activePlayer + 1} moves in ${aiDir}`);
 	if (aiDir === -1) {
 		throw new Error('Ω＼ζ°)ﾁｰﾝ');
@@ -20,4 +27,4 @@ for (;;) {
 	board.moveTo(aiDir);
 }
 
-console.log('ended: hist=', board.trace);
+console.log('ended: hist=', JSON.stringify(board.trace));
