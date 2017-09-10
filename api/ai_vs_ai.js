7e29fd306d6425606d197f6ad973b8cdcb72acb5
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-09-10 17:23:01 +0900

    Measure AI 時間

--- api/ai_vs_ai.js
+++ api/ai_vs_ai.js
@@ -9,6 +9,11 @@ board.on('win', (winner) => {
 	console.log(`AI${winner + 1} won`);
 });
 
+ai1.time = 0;
+ai2.time = 0;
+ai1.n = 0;
+ai2.n = 0;
+
 for (;;) {
 	const point = board.getCurrentPoint();
 	if (!point || point.movableDirections.size === 0) {
@@ -20,7 +25,10 @@ for (;;) {
 	if (board.activePlayer === 0) {
 		hist = hist.map(([x, y]) => [8 - x, 8 - y]);
 	}
+	const start = new Date();
 	let aiDir = ai(hist);
+	ai.time += new Date().valueOf() - start.valueOf();
+	ai.n++;
 	if (aiDir === -1) {
 		throw new Error('Ω＼ζ°)ﾁｰﾝ');
 	}
@@ -30,3 +38,4 @@ for (;;) {
 }
 
 console.log('ended: hist=', JSON.stringify(board.trace));
+console.log(`ai1: avgtime=${ai1.time / ai1.n}; ai2: avgtime=${ai2.time / ai2.n}`);
